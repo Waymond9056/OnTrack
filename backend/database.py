08933df97e43c046334b0fda1fbbf69ea83a4bcf
Mukil Sundaravadivel <mukilsundar@gmail.com>
2025-04-24 14:00:54 -0400

    database wrapper functionality

--- backend/database.py
+++ backend/database.py
@@ -1,18 +1,55 @@
 import os
 from supabase import create_client, Client
-from dotenv import load_dotenv
 
 
-def get_user():
-    load_dotenv()
+
+def create_user(userID):
     url: str = os.getenv("NEXT_PUBLIC_SUPABASE_URL")
     key: str = os.getenv("NEXT_PUBLIC_SUPABASE_ANON_KEY")
     supabase: Client = create_client(url, key)
-
     response = (
         supabase.table("Users")
-        .select("syllabus")
+        .insert({'id': userID})
         .execute()
     )
 
-    return str(response)
+def get_user_data(userID):
+    url: str = os.getenv("NEXT_PUBLIC_SUPABASE_URL")
+    key: str = os.getenv("NEXT_PUBLIC_SUPABASE_ANON_KEY")
+    supabase: Client = create_client(url, key)
+    response = (
+        supabase.table("Users")
+        .select("id")
+        .eq('id', userID)
+        .execute()
+    )
+    return response.data
+
+def get_syllabus(userID):
+    return get_user_data(userID)['syllabus'];
+
+def get_goals(userID):
+    return get_user_data(userID)['goals'];
+
+def get_activities(userID):
+    return get_user_data(userID)['activities'];
+
+def set_user_data(userID, column, newValue):
+    url: str = os.getenv("NEXT_PUBLIC_SUPABASE_URL")
+    key: str = os.getenv("NEXT_PUBLIC_SUPABASE_ANON_KEY")
+    supabase: Client = create_client(url, key)
+    response = (
+        supabase.table("Users")
+        .update({column: newValue})
+        .eq("id", userID)
+        .execute()
+    )
+
+def set_syllabus(userID, newValue):
+    set_user_data(userID, 'syllabus', newValue)
+
+def set_goals(userID, newValue):
+    set_user_data(userID, 'goals', newValue)
+
+def set_activities(userID, newValue):
+    set_user_data(userID, 'activities', newValue)
